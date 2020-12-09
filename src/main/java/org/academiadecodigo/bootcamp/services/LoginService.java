7c36fbe8f01a068c9975ae3c411523f331d209a6
Bruno <olaboatarde@bomdia.com>
2020-12-09 16:34:12 +0000

    Working on Services

--- src/main/java/org/academiadecodigo/bootcamp/services/LoginService.java
+++ src/main/java/org/academiadecodigo/bootcamp/services/LoginService.java
@@ -1,4 +1,18 @@
 package org.academiadecodigo.bootcamp.services;
 
+import org.academiadecodigo.bootcamp.model.User;
+
 public class LoginService {
+
+    private UserService userService;
+
+    //set user
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    user
+
+
+
 }
--- src/main/java/org/academiadecodigo/bootcamp/services/UserService.java
+++ src/main/java/org/academiadecodigo/bootcamp/services/UserService.java
@@ -1,4 +1,28 @@
 package org.academiadecodigo.bootcamp.services;
 
-public class UserService {
+import org.academiadecodigo.bootcamp.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserService implements UserServiceInterface {
+
+    private List<User> usersList = new ArrayList<>();
+
+
+    public List<User> getUsersList() {
+        return usersList;
+    }
+
+    @Override
+    public User getUser(Integer id) {
+        return usersList.get(id);
+    }
+
+    @Override
+    public void add(User user) {
+        usersList.add(user);
+    }
+
+
 }
