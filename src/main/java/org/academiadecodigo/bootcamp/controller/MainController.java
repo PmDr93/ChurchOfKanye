1d5087801071f619450261c71cb6a4cfab51580d
Pedro Rafael <pmdr93@gmail.com>
2020-12-11 04:24:06 +0000

    add movement image11

--- src/main/java/org/academiadecodigo/bootcamp/controller/MainController.java
+++ src/main/java/org/academiadecodigo/bootcamp/controller/MainController.java
@@ -51,7 +51,7 @@ public class MainController {
         user.setUsername("pedro");
         user.setPassword("academia");
         user.setEmail("pedro@gmail.com");
-        user.setMeterPercent(5);
+        user.setMeterPercent(90);
 
         user2.setId(2);
         user2.setFirstName("André");
